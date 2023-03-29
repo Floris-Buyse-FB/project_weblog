b270adb4db67f9a2028a7e8ced375771199116d9
Floris Buyse <floris.buyse@student.hogent.be>
2023-03-29 11:24:38 +0200

    CLI verder gewerkt

--- scripts/AA_CLI.py
+++ scripts/AA_CLI.py
@@ -1,24 +1,38 @@
 import pandas as pd
 import pickle
 import os
+import math
+from sklearn.metrics import accuracy_score
 
 def menu():
     link = str(input("Provide a link to a .csv file with the data you want to predict on:"))
     return link
 
-def predict_robot(data):
+def predict_robot(data, y):
     # load all .sav models from directory modellen
     for file in os.listdir("modellen"):
         if file.endswith(".sav"):
-            model = pickle.load(open("../modellen/" + file, 'rb'))
-            print("Model loaded")
-            print("Predicting...")
-            print(model.predict(data))
+            model = pickle.load(open("./modellen/" + file, 'rb'))
+            print("Model loaded\n------------")
+            print("Predicting...\n-------------")
+            prediction = model.predict(data)
+            perc_hum = (prediction == 0).sum() / len(prediction) * 100
+            perc_rob = (prediction == 1).sum() / len(prediction) * 100
+            print("{:.2f}% Human | {:.2f}% Robot".format(perc_hum, perc_rob))
+            print("Accuracy: {:.2f}".format(((accuracy_score(y, prediction)) * 100)) + "%")
+            print("\n----------------")
+
 
 def main():
-    link = menu()
-    data = pd.read_csv(link)
-    predict_robot(data)
+    df = pd.read_csv("./data/weblogs_copy.csv")
+    df = df.drop(['NIGHT', 'ID', 'OTHER_METHOD'], axis=1)
+
+    X = df.loc[:, df.columns != 'ROBOT']
+    y = df['ROBOT']
+
+    X = X.fillna(0)
+
+    predict_robot(X, y)
 
 if __name__ == "__main__":
 
